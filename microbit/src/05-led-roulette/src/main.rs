68b1d5b28fa5b4355e5391c0d7ded43cfeab9c84
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-06-16 14:21:46 +0700

    make LED light blinking

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,16 +3,28 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
 use microbit::board::Board;
+use microbit::hal::timer::Timer;
 use microbit::hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let mut board = Board::take().unwrap();
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
-    loop {}
+    board.display_pins.col1.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1_000_u16);
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1_000_u16);
+    }
 }
